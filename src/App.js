3d96da6c1502b11751d395a82ddbba035ec50a6e
masanori <masalib@gmail.com>
2020-10-31 06:50:26 +0000

    test commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello,world 2
+          hello,world 3
         </p>
         <a
           className="App-link"
